e9dd0ea2db627801dd55d810f27107ae42b8aed1
jonathan <jonathan@9f0520aa-a7ea-0310-80d7-d2976a5bef54>
2006-01-12 13:32:14 +0000

    allow runConfigure.sh to proceed even if (te)TeX installation not found
    git-svn-id: http://scripts.sil.org/svn-public/xetex/TRUNK@163 9f0520aa-a7ea-0310-80d7-d2976a5bef54

--- runConfigure.sh
+++ runConfigure.sh
@@ -7,20 +7,24 @@ CWD=`pwd`
 test -d Work || mkdir Work
 cd Work
 
+# defaults in case we don't find a teTeX installation
+PREFIX=/usr/local/teTeX
+DATADIR=/usr/local/teTeX/share
+
 # try to figure out where the user's teTeX is, and complain if we can't find it...
 KPSEWHICH=`which kpsewhich`
 if [ ! -e "${KPSEWHICH}" ]; then
 	echo "### No kpsewhich found -- are you sure you have teTeX installed?"
-	exit 1
-fi
-HYPHEN=`kpsewhich hyphen.tex`
-if [ "${HYPHEN}x" == "x" ]; then
-	echo "### hyphen.tex not found -- are you sure you have teTeX installed?"
-	exit 1
+else
+	PREFIX=`echo ${KPSEWHICH} | sed -e 's!/bin/.*!/!;'`
+	HYPHEN=`kpsewhich hyphen.tex`
+	if [ "${HYPHEN}x" == "x" ]; then
+		echo "### hyphen.tex not found -- are you sure you have teTeX installed?"
+	else
+		DATADIR=`echo ${HYPHEN} | sed -e 's!/texmf.*!/!;'`
+	fi
 fi
 
-PREFIX=`echo ${KPSEWHICH} | sed -e 's!/bin/.*!/!;'`
-DATADIR=`echo ${HYPHEN} | sed -e 's!/texmf.*!/!;'`
 
 # run the TeX Live configure script
 echo "### running TeX Live configure script as:"
@@ -48,7 +52,7 @@ mkdir -p TeX/libs/teckit
 )
 
 # To build:
-# "make" in Work/TeX/libs/icu-release-3-4
+# "make" in Work/TeX/libs/icu-release-3-4-source
 # "make" in Work/TeX/libs/teckit
 # "make xetex" in Work/TeX/texk/web2c
 # on Mac OS X only: "make xdv2pdf" in Work/TeX/texk/xdv2pdf
