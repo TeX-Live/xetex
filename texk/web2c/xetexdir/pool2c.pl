d76937eb0db28137a890870b0554ff8e6c8fe389
jonathan <jonathan@9f0520aa-a7ea-0310-80d7-d2976a5bef54>
2007-01-16 23:50:19 +0000

    forgot pool2c in prev commit
    git-svn-id: http://scripts.sil.org/svn-public/xetex/TRUNK@410 9f0520aa-a7ea-0310-80d7-d2976a5bef54

---
+++ texk/web2c/xetexdir/pool2c.pl
@@ -0,0 +1,64 @@
+#! /usr/bin/perl
+
+# convert web .pool file into an array of C strings
+# usage: perl pool2c.pl NAME.pool > NAME_pool.c
+
+die "expected pool name as argument" unless $#ARGV == 0;
+
+$prog = `basename $ARGV[0] .pool`;
+chomp($prog);
+
+open IN, "< $ARGV[0]" or die "can't open input $ARGV[0]";
+
+print <<__EOT__;
+/* This file is auto-generated from $ARGV[0] by pool2c.pl */
+#define EXTERN extern
+#include "${prog}d.h"
+
+#include <stdio.h>
+
+static unsigned char POOL[] =
+__EOT__
+$pool_len = 0;
+while (<IN>) {
+	chomp;
+	if (m/^([0-9]{2})(.*)$/) {
+		($len, $str) = ($1, $2);
+		if (length($str) != $len) {
+			die "*** pool string length error:\n$_\n";
+		}
+		$str =~ s/(["\\])/\\$1/g;
+		$str =~ s/\?\?\?/\\?\\?\\?/g;
+		printf "\t\"\\x%02X\" \"%s\"\n", $len, $str;
+		$pool_len += $len + 1;
+	}
+	elsif (m/^\*[0-9]+$/) {
+		last;
+	}
+	else {
+		die "*** unexpected line in pool file:\n$_\n"; 
+	}
+}
+
+close IN;
+
+print <<__EOT__;
+	;
+
+#define POOL_LEN $pool_len
+
+int initpool(int limit) {
+	int g = 0;
+	int i = 0, j = 0;
+	unsigned char *s = POOL;
+	while (s < POOL + POOL_LEN) {
+		int len = *s++;
+		if (poolptr + len >= limit)
+			return 0;
+		while (len-- > 0)
+			strpool[poolptr++] = *s++;
+		g = makestring();
+	}
+	return g;
+}
+__EOT__
