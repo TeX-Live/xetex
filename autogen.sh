911943a3240154dffa93cb35785c69dacfdacecb
Khaled Hosny <khaledhosny@eglug.org>
2012-12-02 03:13:13 +0200

    Slightly smarter autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,1 +1,23 @@
-autoreconf --force --install --verbose
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd $srcdir
+
+echo -n "checking for pkg-config... "
+which pkg-config || {
+	echo "*** No pkg-config found, please install it ***"
+	exit 1
+}
+
+echo -n "checking for autoreconf... "
+which autoreconf || {
+	echo "*** No autoreconf found, please install it ***"
+	exit 1
+}
+
+echo "running autoreconf --force --install --verbose"
+autoreconf --force --install --verbose || exit $?
