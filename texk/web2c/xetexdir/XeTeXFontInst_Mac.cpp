bc4970bad9a88d55ed35a0de9d95ced508a199df
Khaled Hosny <khaledhosny@eglug.org>
2012-12-04 03:25:46 +0100

    Make OT layout work on Mac
    Add glue code for using HarfBuzz with ATS API. We probably should use
    FreeType here too, but this should suffice for now.

--- texk/web2c/xetexdir/XeTeXFontInst_Mac.cpp
+++ texk/web2c/xetexdir/XeTeXFontInst_Mac.cpp
@@ -61,6 +61,52 @@ XeTeXFontInst_Mac::~XeTeXFontInst_Mac()
 		ATSUDisposeStyle(fStyle);
 }
 
+static hb_bool_t _get_glyph(hb_font_t* hbFont, void* fontData, hb_codepoint_t unicode, hb_codepoint_t variationSelector, hb_codepoint_t* glyph, void* userData)
+{
+	*glyph = MapCharToGlyph_AAT((ATSUStyle) fontData, unicode);
+	return !!glyph;
+}
+
+static hb_position_t _get_glyph_h_advance(hb_font_t* hbFont, void* fontData, hb_codepoint_t glyph, void* userData)
+{
+	float width = GetGlyphWidth_AAT((ATSUStyle) fontData, glyph);
+	return width * 64;
+}
+
+static hb_bool_t _get_glyph_extents(hb_font_t* hbFont, void* fontData, hb_codepoint_t glyph, hb_glyph_extents_t* extents, void* userData)
+{
+	GlyphBBox bbox;
+	GetGlyphBBox_AAT((ATSUStyle) fontData, glyph, &bbox);
+	extents->x_bearing = bbox.xMin;
+	extents->y_bearing = bbox.yMax;
+	extents->width = bbox.xMax - bbox.xMin;
+	extents->height = bbox.yMax - bbox.yMin;
+
+	return true;
+}
+
+static hb_font_funcs_t* _get_font_funcs()
+{
+	static hb_font_funcs_t* _font_funcs = 0;
+	_font_funcs = hb_font_funcs_create();
+	hb_font_funcs_set_glyph_func(_font_funcs, _get_glyph, 0, 0);
+	hb_font_funcs_set_glyph_h_advance_func(_font_funcs, _get_glyph_h_advance, 0, 0);
+	hb_font_funcs_set_glyph_extents_func(_font_funcs, _get_glyph_extents, 0, 0);
+	hb_font_funcs_make_immutable(_font_funcs);
+
+	return _font_funcs;
+}
+
+static hb_blob_t* _get_font_table(hb_face_t* face, hb_tag_t tag, void* userData)
+{
+	XeTeXFontInst* font = (XeTeXFontInst*) userData;
+	uint32_t length;
+	const void *table = font->getFontTable((OTTag) tag, &length);
+	if (!table || !length)
+		return 0;
+	return hb_blob_create((const char*) table, length, HB_MEMORY_MODE_READONLY, NULL, NULL);
+}
+
 void XeTeXFontInst_Mac::initialize(int &status)
 {
     if (fFontRef == 0) {
@@ -81,6 +127,14 @@ void XeTeXFontInst_Mac::initialize(int &status)
 		ByteCount				valueSizes[3] = { sizeof(ATSUFontID), sizeof(Fixed), sizeof(ATSStyleRenderingOptions) };
 		ATSUAttributeValuePtr	values[3] = { &font, &size, &options };
 		ATSUSetAttributes(fStyle, 3, tags, valueSizes, values);
+
+		/* for HarfBuzz */
+		hb_face_t* lFace = hb_face_create_for_tables(_get_font_table, this, 0);
+		hb_face_set_upem (lFace, fUnitsPerEM);
+		hbFont = hb_font_create(lFace);
+		hb_font_set_funcs(hbFont, _get_font_funcs(), fStyle, 0);
+		hb_font_set_scale(hbFont, fPointSize, fPointSize);
+		hb_font_set_ppem(hbFont, fPointSize, fPointSize);
 	}
 	else {
 		status = 1;
