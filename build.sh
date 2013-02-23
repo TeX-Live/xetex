4d8d55a739205701a8153ffe350f6e8b309eb93b
Khaled Hosny <khaledhosny@eglug.org>
2013-02-23 06:07:39 +0200

    Add --debug option from luatex's build.sh

--- build.sh
+++ build.sh
@@ -28,6 +28,7 @@
 #      --host=     : target system for mingw32 cross-compilation
 #      --build=    : build system for mingw32 cross-compilation
 #      --arch=     : crosscompile for ARCH on OS X
+#      --debug     : CFLAGS="-g -O0" --warnings=max --nostrip
 $DEBUG
 
 # try to find bash, in case the standard shell is not capable of
@@ -70,6 +71,7 @@ until [ -z "$1" ]; do
   case "$1" in
     --make      ) ONLY_MAKE=TRUE     ;;
     --nostrip   ) STRIP_XETEX=FALSE ;;
+    --debug     ) STRIP_XETEX=FALSE; WARNINGS=max ; CFLAGS="-g -O0 $CFLAGS" ;;
     --syslibs   ) SYSTEM_LIBS=TRUE   ;;
     --warnings=*) WARNINGS=`echo $1 | sed 's/--warnings=\(.*\)/\1/' `        ;;
     --mingw     ) MINGWCROSS=TRUE    ;;
@@ -125,6 +127,10 @@ then
   export CFLAGS CXXFLAGS OBJCXXFLAGS LDFLAGS
 fi
 
+if [ "$STRIP_XETEX" = "FALSE" ]
+then
+    export CFLAGS
+fi
 
 # ----------
 # clean up, if needed
