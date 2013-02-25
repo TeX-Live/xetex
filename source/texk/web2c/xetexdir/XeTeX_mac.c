7df9e42ca7d68c717436a5608cd4a9e234c1f94f
Khaled Hosny <khaledhosny@eglug.org>
2013-02-25 14:22:42 +0100

    Just use glyph 0 here

--- source/texk/web2c/xetexdir/XeTeX_mac.c
+++ source/texk/web2c/xetexdir/XeTeX_mac.c
@@ -152,13 +152,13 @@ DoAtsuiLayout(void* p, int justify)
 		for (j = 0; j < count; j++) {
 			if (glyphs[j] < 0xfffe) {
 				// XXX Core Text has that font cascading thing that will do
-				// font substitution for missing glyphs, which do not want but
-				// I can not find a way to disable it yet, so if the font of
-				// the resulting run is not the same font we asked for, use the
-				// glyph for space instead or we will be showing garbage or
-				// even invalid glyphs
+				// font substitution for missing glyphs, which we do not want
+				// but I can not find a way to disable it yet, so if the font
+				// of the resulting run is not the same font we asked for, use
+				// the glyph at index 0 (usually .notdef) instead or we will be
+				// showing garbage or even invalid glyphs
 				if (fontFromAttributes(attributes) != fontFromAttributes(runAttr))
-					realGlyphIDs[realGlyphCount] = MapCharToGlyph_AAT(attributes, ' ');
+					realGlyphIDs[realGlyphCount] = 0;
 				else
 					realGlyphIDs[realGlyphCount] = glyphs[j];
 				locations[realGlyphCount].x = FixedPStoTeXPoints(positions[j].x) + lsDelta;
