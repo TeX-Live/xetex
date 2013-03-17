TeX-Live/xetex

commit ce08d998791dd042f25a96713d1f601ebc088445

Don't call loadAAT() if createFont() returned NULL
If createFont() returned NULL this could mean that FreeType failed to
load the font, though Core Text might still be able to load the font, it
will fail later in xdvipdfmx which must open the font with FreeType, so
we better fail early.

source/texk/web2c/xetexdir/XeTeX_ext.c
@@ -1255,7 +1255,7 @@ findnativefont(unsigned char* uname, integer scaled_size)
 #endif
 
 			font = createFont(fontRef, scaled_size);
-			if (font != 0) {
+			if (font != NULL) {
 #ifdef XETEX_MAC
 				if (getReqEngine() == 'O' || getReqEngine() == 'G' ||
 					getFontTablePtr(font, kGSUB) != NULL || getFontTablePtr(font, kGPOS) != NULL)
@@ -1267,8 +1267,10 @@ findnativefont(unsigned char* uname, integer scaled_size)
 	
 #ifdef XETEX_MAC
 			if (rval == NULL) {
-			load_aat:
-				rval = loadAATfont(fontRef, scaled_size, featString);
+load_aat:
+				if (font != NULL) {
+					rval = loadAATfont(fontRef, scaled_size, featString);
+				}
 			}
 #endif
 

commit 4e96e8332ff5224e2beed24a05ccff4eb8710918

Fix font not found errors on PPC Mac
No idea why this didn’t fail on other architectures, it was brain dead
broken since ce08d99.

source/texk/web2c/xetexdir/XeTeX_ext.c
@@ -1148,7 +1148,7 @@ findnativefont(unsigned char* uname, integer scaled_size)
 	char*	varString = NULL;
 	char*	featString = NULL;
 	PlatformFontRef	fontRef;
-	XeTeXFont	font;
+	XeTeXFont	font = NULL;
 	int		index = 0;
 
 	loadedfontmapping = NULL;
@@ -1246,13 +1246,13 @@ findnativefont(unsigned char* uname, integer scaled_size)
 				}
 			}
 
+			font = createFont(fontRef, scaled_size);
 #ifdef XETEX_MAC
 			/* decide whether to use AAT or OpenType rendering with this font */
 			if (getReqEngine() == 'A')
 				goto load_aat;
 #endif
 
-			font = createFont(fontRef, scaled_size);
 			if (font != NULL) {
 #ifdef XETEX_MAC
 				if (getReqEngine() == 'O' || getReqEngine() == 'G' ||