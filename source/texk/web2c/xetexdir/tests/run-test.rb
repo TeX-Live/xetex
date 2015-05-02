da03f7a152bc75e8df25ef631e2376e7cc302a43
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2015-05-02 15:45:53 +0100

    Extending TEXINPUTS in run-test.rb

--- source/texk/web2c/xetexdir/tests/run-test.rb
+++ source/texk/web2c/xetexdir/tests/run-test.rb
@@ -2,6 +2,8 @@
 
 srcdir = File.expand_path('../../../../kpathsea', __FILE__)
 ENV['TEXMFCNF'] = srcdir
-ENV['TEXINPUTS'] = File.expand_path('../../../tests', __FILE__) + ':'
+ENV['TEXINPUTS'] = ['../../../tests', '../../../xetexdir/tests'].map do |relpath|
+  File.expand_path(relpath, __FILE__)
+end.join(':') + ':'
 xetexbin = File.expand_path('../../../../../../build/texk/web2c/xetex', __FILE__)
 system("#{xetexbin} -etex -ini -no-pdf #{ARGV.first}")
