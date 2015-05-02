6baa32861d49f29e9367530be663776f6edb77ad
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2015-05-02 01:56:57 +0100

    Disabling xdvipdfmx in test run until I can find a better solution.

--- source/texk/web2c/xetexdir/tests/run-test.rb
+++ source/texk/web2c/xetexdir/tests/run-test.rb
@@ -4,4 +4,4 @@ srcdir = File.expand_path('../../../../kpathsea', __FILE__)
 ENV['TEXMFCNF'] = srcdir
 ENV['TEXINPUTS'] = File.expand_path('../../../tests', __FILE__) + ':'
 xetexbin = File.expand_path('../../../../../../build/texk/web2c/xetex', __FILE__)
-system("#{xetexbin} -etex -ini #{ARGV.first}")
+system("#{xetexbin} -etex -ini -no-pdf #{ARGV.first}")
