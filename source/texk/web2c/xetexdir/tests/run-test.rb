07e26df035288b91919bd094c188fe8353edf7ab
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2015-08-23 18:56:58 +0100

    Simplified to the extreme.

--- source/texk/web2c/xetexdir/tests/run-test.rb
+++ source/texk/web2c/xetexdir/tests/run-test.rb
@@ -1,9 +1,17 @@
 #!/usr/bin/env ruby
 
+la = if ARGV.first == '-l' then 'la' else '' end
+if ARGV.first == '-l' then filename = ARGV[1] else filename = ARGV.first end
+
+puts "Filename is #{filename}"
+
 srcdir = File.expand_path('../../../../kpathsea', __FILE__)
 ENV['TEXMFCNF'] = srcdir
 ENV['TEXINPUTS'] = ['../../../tests', '../../../xetexdir/tests'].map do |relpath|
   File.expand_path(relpath, __FILE__)
 end.join(':') + ':'
-xetexbin = File.expand_path('../../../../../../build/texk/web2c/xetex', __FILE__)
-system("#{xetexbin} -etex -ini -no-pdf #{ARGV.first}")
+puts "export TEXMFCNF=#{ENV['TEXMFCNF']}"
+puts "export TEXINPUTS=#{ENV['TEXINPUTS']}"
+xetexbin = File.expand_path("../../../../../../build/texk/web2c/xe#{la}tex", __FILE__)
+puts "Running: #{xetexbin} -etex -ini -no-pdf #{filename}"
+system("#{xetexbin} -etex -ini -no-pdf #{filename}")
