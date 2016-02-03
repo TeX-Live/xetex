dfde6b67898eed90428a4b1d847efddc78aea256
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2016-02-03 19:57:17 +0000

    Attempt to make test case for bug #117 really independent.  Needs post-TeX Live 2015 xdvipdfmx.

--- source/texk/web2c/xetexdir/tests/run-test.rb
+++ source/texk/web2c/xetexdir/tests/run-test.rb
@@ -10,8 +10,14 @@ ENV['TEXMFCNF'] = srcdir
 ENV['TEXINPUTS'] = ['../../../tests', '../../../xetexdir/tests'].map do |relpath|
   File.expand_path(relpath, __FILE__)
 end.join(':') + ':'
+ENV['OSFONTDIR'] = File.expand_path('../fonts', __FILE__)
+ENV['TFMFONTS'] = '/usr/local/texlive/2015/texmf-dist/fonts/tfm//:'
 puts "export TEXMFCNF=#{ENV['TEXMFCNF']}"
 puts "export TEXINPUTS=#{ENV['TEXINPUTS']}"
+puts "export OSFONTDIR=#{ENV['OSFONTDIR']}"
+puts "export TFMFONTS=#{ENV['TFMFONTS']}"
+puts `kpsewhich latinmodern-math.otf`
+puts 'cmex10:' + `kpsewhich cmex10.tfm`
 xetexbin = File.expand_path("../../../../../../build/texk/web2c/xe#{la}tex", __FILE__)
 puts "Running: #{xetexbin} -etex -ini -no-pdf #{filename}"
 system("#{xetexbin} -etex -ini -no-pdf #{filename}")
